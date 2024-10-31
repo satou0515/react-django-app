2471d9d31cb1ef7f297b0e7c556c2a209a7054fc
satou0515 <satsuki515.503.503@gmail.com>
2024-10-31 23:18:15 +0900

    ログイン周りの画面UI修正

--- frontend/src/page/auth/Login.js
+++ frontend/src/page/auth/Login.js
@@ -41,9 +41,9 @@ const Login = (props) => {
           <div className="h-full max-w-[850px] flex-grow mx-24 px-24">
             <div className="h-full flex flex-col justify-center mx-24">
               <h2 className="text-2xl font-bold text-center">ログイン</h2>
-              <form className="w-full space-y-6 py-4">
+              <form className="w-full space-y-3 py-4 text-xs">
                 <div className="mt-2">
-                  <label className="block text-sm font-medium text-gray-700">
+                  <label className="block font-medium text-gray-700">
                     アカウント名／メールアドレス<span className="pl-4 text-red-500">必須</span>
                   </label>
                   <input
@@ -53,11 +53,11 @@ const Login = (props) => {
                     value={email}
                     onChange={handleEmailChange}
                     required
-                    className="w-full p-3 mt-1 border-2 border-gray-500 rounded-lg foucus:outline-none foucus:border-blue-300"
+                    className="w-full p-2 mt-1 border-2 border-gray-500 rounded-lg foucus:outline-none foucus:border-blue-300"
                   />
                 </div>
                 <div className="mt-2">
-                  <label className="block text-sm font-midium text-gray-700">
+                  <label className="block font-midium text-gray-700">
                     パスワード<span className="pl-4 text-red-500">必須</span>
                   </label>
                   <input
@@ -67,14 +67,14 @@ const Login = (props) => {
                     value={password}
                     onChange={handlePasswordChange}
                     required
-                    className="w-full p-3 mt-1 border-2 border-gray-500 rounded-lg foucus:outline-none foucus:border-blue-300"
+                    className="w-full p-2 mt-1 border-2 border-gray-500 rounded-lg foucus:outline-none foucus:border-blue-300"
                   />
                 </div>
-                <div className="mt-8">
+                <div>
                   <button
                     type="submit"
                     onClick={handleSubmit}
-                    className="w-full py-3 text-white bg-blue-500 rounded-full hover:bg-blue-700"
+                    className="w-full mt-4 py-3 text-white bg-blue-500 rounded-full hover:bg-blue-700"
                   >
                     ログイン
                   </button>
--- frontend/src/page/auth/Register.js
+++ frontend/src/page/auth/Register.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 import { Container } from "@mui/material";
 
 const Register = (props) => {
+  const [avatar, setAvatar] = useState(null);
+  const [avatarPreview, setAvatarPreview] = useState(''); // プレビュー用
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -9,20 +11,46 @@ const Register = (props) => {
   const [birthday, setBirthday] = useState('');
   const [sex, setSex] = useState('');
 
+  const handleAvatarChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setAvatar(file);
+      setAvatarPreview(URL.createObjectURL(file)); // プレビュー用URLを設定
+    }
+  };
+
   const handleSubmit = async(e) => {
     e.preventDefault();
 
     const emailInput = document.getElementById('email');
     // バリデーションチェック
+    if (password !== validPassword) {
+      console.log('Error: Passwords do not match');
+      return;
+    }
     if(!emailInput.checkVisibility()) {
       console.log('Error: ', emailInput.validationMessage);
     }else {
-      try {
-        // const response = await axios.post(`${apiUrl}/api/authentication/forget-password/`, { email });
-        console.log('成功！');
-      }catch(error) {
-        console.log('Error: ', error);
-      }
+      return;
+    }
+
+    // FormDataオブジェクトを作成して画像ファイルと他のデータを追加
+    const formData = new FormData();
+    formData.append('avatar', avatar); // 画像ファイルを追加
+    formData.append('name', name);
+    formData.append('email', email);
+    formData.append('password', password);
+    formData.append('birthday', birthday);
+    formData.append('sex', sex);
+    try {
+      // const response = await axios.post(`${apiUrl}/api/authentication/forget-password/`, formData, {
+      //   headers: {
+      //     'Content-Type': 'multipart/form-data',
+      //   },
+      // });
+      console.log('成功！');
+    }catch(error) {
+      console.log('Error: ', error);
     }
   }
 
@@ -33,90 +61,112 @@ const Register = (props) => {
           <div className="h-full max-w-[850px] flex-grow mx-24 px-24">
             <div className="h-full flex flex-col justify-center mx-24">
             <h2 className="text-2xl font-bold text-center">アカウント作成</h2>
-              <form className="w-full space-y-6 py-4" onSubmit={handleSubmit} noValidate>
+              <form className="w-full space-y-3 py-4 text-xs" onSubmit={handleSubmit} noValidate>
+                <div className='mt-2 flex justify-center relative'>
+                  <input
+                    id='avatar'
+                    name='avatar'
+                    type='file'
+                    accept='image/*'
+                    onChange={handleAvatarChange}
+                    className="absolute opacity-0 w-24 h-24 rounded-full cursor-pointer"
+                    required
+                  />
+                  {avatarPreview ? (
+                    <img
+                      src={avatarPreview}
+                      alt="Avatar Preview"
+                      className="w-24 h-24 rounded-full border-2 border-gray-500 object-cover"
+                    />
+                  ) : (
+                    <div className="w-24 h-24 rounded-full border-2 border-gray-500 flex items-center justify-center text-gray-500">
+                      No Image
+                    </div>
+                  )}
+                </div>
                 <div className='mt-2'>
-                  <label className="block text-sm font-medium text-gray-700">アカウント名</label>
+                  <label className="block font-medium text-gray-700">アカウント名<span className='pl-4 text-red-500'>必須</span></label>
                   <input
                     id='name'
                     name='name'
                     type='text'
                     value={name}
                     onChange={(e) => setName(e.target.value)}
-                    className="w-full p-3 mt-1 border-2 border-gray-500 rounded-lg foucus:outline-none foucus:border-blue-300"
+                    className="w-full p-2 mt-1 border-2 border-gray-500 rounded-lg foucus:outline-none foucus:border-blue-300"
                     required
                   />
                 </div>
                 <div>
-                  <label className="block text-sm font-medium text-gray-700">メールアドレス</label>
+                  <label className="block font-medium text-gray-700">メールアドレス<span className='pl-4 text-red-500'>必須</span></label>
                   <input
                     id='email'
                     name='email'
                     type='email'
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
-                    className="w-full p-3 mt-1 border-2 border-gray-500 rounded-lg foucus:outline-none foucus:border-blue-300"
+                    className="w-full p-2 mt-1 border-2 border-gray-500 rounded-lg foucus:outline-none foucus:border-blue-300"
                     required
                   />
                 </div>
                 <div>
-                  <label className="block text-sm font-medium text-gray-700">新しいパスワード<span className='pl-4 text-red-500'>必須</span></label>
+                  <label className="block font-medium text-gray-700">新しいパスワード<span className='pl-4 text-red-500'>必須</span></label>
                   <input
                     id='password'
                     name='password'
                     type='password'
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
-                    className="w-full p-3 mt-1 border-2 border-gray-500 rounded-lg foucus:outline-none foucus:border-blue-300"
+                    className="w-full p-2 mt-1 border-2 border-gray-500 rounded-lg foucus:outline-none foucus:border-blue-300"
                     required
                   />
                 </div>
                 <div>
-                  <label className="block text-sm font-medium text-gray-700">新しいパスワード（確認）<span className='pl-4 text-red-500'>必須</span></label>
+                  <label className="block font-medium text-gray-700">新しいパスワード（確認）<span className='pl-4 text-red-500'>必須</span></label>
                   <input
                     id='validPassword'
                     name='validPassword'
                     type='validPassword'
                     value={validPassword}
                     onChange={(e) => setValidPassword(e.target.value)}
-                    className="w-full p-3 mt-1 border-2 border-gray-500 rounded-lg foucus:outline-none foucus:border-blue-300"
+                    className="w-full p-2 mt-1 border-2 border-gray-500 rounded-lg foucus:outline-none foucus:border-blue-300"
                     required
                   />
                 </div>
                 <div>
-                  <label className="block text-sm font-medium text-gray-700">生年月日</label>
+                  <label className="block font-medium text-gray-700">生年月日</label>
                   <input
                     id='birthday'
                     name='birthday'
                     type='text'
                     value={birthday}
                     onChange={(e) => setBirthday(e.target.value)}
-                    className="w-full p-3 mt-1 border-2 border-gray-500 rounded-lg foucus:outline-none foucus:border-blue-300"
+                    className="w-full p-2 mt-1 border-2 border-gray-500 rounded-lg foucus:outline-none foucus:border-blue-300"
                     required
                   />
                 </div>
                 <div>
-                  <label className="block text-sm font-medium text-gray-700">性別</label>
+                  <label className="block font-medium text-gray-700">性別</label>
                   <input
                     id='sex'
                     name='sex'
                     type='text'
                     value={sex}
                     onChange={(e) => setSex(e.target.value)}
-                    className="w-full p-3 mt-1 border-2 border-gray-500 rounded-lg foucus:outline-none foucus:border-blue-300"
+                    className="w-full p-2 mt-1 border-2 border-gray-500 rounded-lg foucus:outline-none foucus:border-blue-300"
                     required
                   />
                 </div>
-                <div className="mt-8">
+                <div>
                   <button
                     type="submit"
                     onClick={handleSubmit}
-                    className="w-full py-3 text-white bg-blue-500 rounded-full hover:bg-blue-700"
+                    className="w-full mt-4 py-3 font-bold text-sm text-white bg-blue-500 rounded-full hover:bg-blue-700"
                   >
                     アカウントを作成
                   </button>
                 </div>
               </form>
-              <div className="mt-4 text-center">
+              <div className="text-center">
                 <a href="/login" className="text-sm text-blue-500 under-line">戻る</a>
               </div>
             </div>
