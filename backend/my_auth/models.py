d6ec5630611f40aebf107ba7f7dd361ce1ab8971
satou0515 <satsuki515.503.503@gmail.com>
2024-11-03 06:29:29 +0900

    テーブル作成手前まで完了

--- backend/my_auth/models.py
+++ backend/my_auth/models.py
@@ -5,19 +5,11 @@ import uuid
 
 class UserInformation(AbstractUser):
   firebase_uid = models.CharField(max_length=255, unique=True)
-  user_id = models.CharField(max_length=255, unique=True)
   account_name = models.CharField(max_length=50, unique=True)
   birth_date = models.DateField(null=True, blank=True)
   GENDER_CHOICES = [('male', '男性'), ('female', '女性'), ('other', 'その他')]
-  sex = models.CharField(max_length=10, choices=GENDER_CHOICES, blank=True)
+  gender = models.CharField(max_length=10, choices=GENDER_CHOICES, blank=True)
   icon_image = models.ImageField(upload_to='user_icons/', null=True, blank=True)
 
   class Meta:
     db_table = 'user_information'
-
-  def generate_unique_user_id(self):
-    while True:
-      user_id = uuid.uuid4().hex[:8].upper()
-      if not UserInformation.objects.filter(user_id=user_id).exists():
-        break
-    return user_id
--- backend/my_auth/serializers.py
+++ backend/my_auth/serializers.py
@@ -2,9 +2,12 @@ from rest_framework import serializers
 from my_auth.models import UserInformation
 
 class UserInformationSerializer(serializers.ModelSerializer):
-  account_name = serializers.CharField(max_length=50)
-  email = serializers.EmailField()
-  password = serializers.CharField()
-  birth_date = serializers.DateField(required=False, allow_null=True)
-  sex = serializers.ChoiceField(choices=UserInformation.GENDER_CHOICES, required=False, allow_null=True)
-  icon_image = serializers.ImageField(required=False, allow_null=True)
+  class Meta:
+    model = UserInformation
+    fields = ['firebase_uid', 'account_name', 'birth_date', 'gender', 'icon_image']
+
+  def create(self, validated_data):
+    user = UserInformation(**validated_data)
+    user.set_password(validated_data['password'])  # パスワードをハッシュ化
+    user.save()
+    return user
